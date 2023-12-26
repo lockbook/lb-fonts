64a26a3d6545d48eedd6548335f1fbe9a8e23a47
ad-tra <aew31gcmuy@gmail.com>
2023-12-26 01:15:17 -0500

    add material symbols

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub const PT_MONO_REGULAR: &[u8] = include_bytes!("../fonts/PTMono-Regular.ttf")
 pub const PT_SANS_BOLD: &[u8] = include_bytes!("../fonts/PTSans-Bold.ttf");
 pub const MATERIAL_ICONS_OUTLINED_REGULAR: &[u8] =
     include_bytes!("../fonts/material-icons-outlined-regular.otf");
+pub const MATERIAL_SYMBOLS_OUTLINED: &[u8] =
+    include_bytes!("../fonts/material-symbols-outlined-4-0-0.ttf");
