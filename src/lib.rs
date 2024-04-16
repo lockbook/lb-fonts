e959233c1e357ea3799c4bc20900350e1e47d137
Smail Barkouch <smailbarkouch1@gmail.com>
2024-04-16 13:47:50 -0400

    added android fonts

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 pub const PT_SANS_REGULAR: &[u8] = include_bytes!("../fonts/PTSans-Regular.ttf");
 pub const PT_MONO_REGULAR: &[u8] = include_bytes!("../fonts/PTMono-Regular.ttf");
 pub const PT_SANS_BOLD: &[u8] = include_bytes!("../fonts/PTSans-Bold.ttf");
+
 pub const MATERIAL_ICONS_OUTLINED_REGULAR: &[u8] =
     include_bytes!("../fonts/material-icons-outlined-regular.otf");
 pub const MATERIAL_SYMBOLS_OUTLINED: &[u8] =
     include_bytes!("../fonts/material-symbols-outlined-4-0-0.ttf");
+
 pub const SF_MONO_REGULAR: &[u8] = include_bytes!("../fonts/SF-Mono-Regular.otf");
 pub const SF_PRO_TEXT_REGULAR: &[u8] = include_bytes!("../fonts/SF-Pro.ttf");
 pub const SF_PRO_TEXT_BOLD: &[u8] = include_bytes!("../fonts/SF-Pro-Text-Bold.otf");
+
+pub const ROBOTO_REGULAR: &[u8] = include_bytes!("../fonts/Roboto-Regular.ttf");
+pub const ROBOTO_BOLD: &[u8] = include_bytes!("../fonts/Roboto-Bold.ttf");
+pub const ROBOTO_MONO_REGULAR: &[u8] = include_bytes!("../fonts/RobotoMono-Regular.ttf");
