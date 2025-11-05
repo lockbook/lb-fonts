07e3e38ae9563d4817907f7f3c276483b9849746
parth <parth@mehrotra.me>
2025-11-05 16:08:36 -0500

    nerd-fonts

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
 pub const PT_SANS_REGULAR: &[u8] = include_bytes!("../fonts/PTSans-Regular.ttf");
-pub const PT_MONO_REGULAR: &[u8] = include_bytes!("../fonts/PTMono-Regular.ttf");
+// pub const PT_MONO_REGULAR: &[u8] = include_bytes!("../fonts/PTMono-Regular.ttf");
 pub const PT_SANS_BOLD: &[u8] = include_bytes!("../fonts/PTSans-Bold.ttf");
-
-pub const MATERIAL_ICONS_OUTLINED_REGULAR: &[u8] =
-    include_bytes!("../fonts/material-icons-outlined-regular.otf");
-pub const MATERIAL_SYMBOLS_OUTLINED: &[u8] =
-    include_bytes!("../fonts/material-symbols-outlined-4-0-0.ttf");
 
 pub const SF_MONO_REGULAR: &[u8] = include_bytes!("../fonts/SF-Mono-Regular.otf");
 pub const SF_PRO_REGULAR: &[u8] = include_bytes!("../fonts/SF-Pro.ttf");
 pub const SF_PRO_TEXT_BOLD: &[u8] = include_bytes!("../fonts/SF-Pro-Text-Bold.otf");
 
-pub const ROBOTO_REGULAR: &[u8] = include_bytes!("../fonts/Roboto-Regular.ttf");
-pub const ROBOTO_BOLD: &[u8] = include_bytes!("../fonts/Roboto-Bold.ttf");
-pub const ROBOTO_MONO_REGULAR: &[u8] = include_bytes!("../fonts/RobotoMono-Regular.ttf");
+pub const NERD_FONTS_SYMBOLS: &[u8] = include_bytes!("../fonts/SymbolsNerdFont-Regular.ttf");
 
 pub const TWITTER_COLOR_EMOJI_15_1: &[u8] =
     include_bytes!("../fonts/TwitterColorEmoji-SVGinOT.ttf");
