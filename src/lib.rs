157e66fb1bc0eda18a24eb0544701bcd1352f542
Travis Vanderstad <t.vanderstad@gmail.com>
2026-03-06 01:35:02 -0500

    add mozilla twitter emojis and italic fonts

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 pub const PT_SANS_REGULAR: &[u8] = include_bytes!("../fonts/PTSans-Regular.ttf");
 // pub const PT_MONO_REGULAR: &[u8] = include_bytes!("../fonts/PTMono-Regular.ttf");
 pub const PT_SANS_BOLD: &[u8] = include_bytes!("../fonts/PTSans-Bold.ttf");
+pub const PT_SANS_ITALIC: &[u8] = include_bytes!("../fonts/PTSans-Italic.ttf");
 
 pub const SF_MONO_REGULAR: &[u8] = include_bytes!("../fonts/SF-Mono-Regular.otf");
 pub const SF_PRO_REGULAR: &[u8] = include_bytes!("../fonts/SF-Pro.ttf");
 pub const SF_PRO_TEXT_BOLD: &[u8] = include_bytes!("../fonts/SF-Pro-Text-Bold.otf");
+pub const SF_PRO_ITALIC: &[u8] = include_bytes!("../fonts/SF-Pro-Italic.ttf");
 
-pub const NERD_FONTS_MONO_SYMBOLS: &[u8] = include_bytes!("../fonts/SymbolsNerdFontMono-Regular.ttf");
+pub const NERD_FONTS_MONO_SYMBOLS: &[u8] =
+    include_bytes!("../fonts/SymbolsNerdFontMono-Regular.ttf");
 pub const NERD_FONTS_SYMBOLS: &[u8] = include_bytes!("../fonts/SymbolsNerdFont-Regular.ttf");
 
-pub const TWITTER_COLOR_EMOJI_15_1: &[u8] =
-    include_bytes!("../fonts/TwitterColorEmoji-SVGinOT.ttf");
+pub const TWEMOJI_MOZILLA: &[u8] =
+    include_bytes!("../fonts/TwemojiMozilla.ttf");
 
 pub const JETBRAINS_MONO: &[u8] = include_bytes!("../fonts/JetBrainsMono-VariableFont_wght.ttf");
